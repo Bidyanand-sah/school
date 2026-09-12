86c6131a7ac86e0e308a7bd3e98e568dab9c000f
Bidyanand <bidyanandk506@gmail.com>
2026-09-12 09:19:00 +0530

    add config.php in auth_check.php

--- frontend/comp/auth_check.php
+++ frontend/comp/auth_check.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once __DIR__ . '/../../backend/config.php';
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header("Location: " . app_url('/backend/login/login.php'));
     exit;
